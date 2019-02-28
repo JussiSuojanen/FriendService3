ec16f32782f605c24ca0400e2e6d4acbdd6af69f
Jussi Suojanen <jussi.suojanen@vincit.fi>
2019-02-28 18:23:56 +0200

    Add Friend model

---
+++ Sources/App/Models/Friend.swift
@@ -0,0 +1,34 @@
+//
+//  Friend.swift
+//  App
+//
+//  Created by Jussi Suojanen on 16/12/2018.
+//
+
+import FluentSQLite
+import Vapor
+
+final class Friend: Codable {
+    var id: Int?
+    let firstname: String
+    let lastname: String
+    let phonenumber: String
+
+    init(id: Int?, firstname: String, lastname: String, phonenumber: String) {
+        self.id = id
+        self.firstname = firstname
+        self.lastname = lastname
+        self.phonenumber = phonenumber
+    }
+}
+
+extension Friend: SQLiteModel { }
+
+/// Allows `Friend` to be used as a dynamic migration.
+extension Friend: Migration { }
+
+/// Allows `Friend` to be encoded to and decoded from HTTP messages.
+extension Friend: Content { }
+
+/// Allows `Friend` to be used as a dynamic parameter in route definitions.
+extension Friend: Parameter { }
